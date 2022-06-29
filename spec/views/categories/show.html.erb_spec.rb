5fc9f0090f89ccc84a0e674c78872dcc8c01e818
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-06-29 14:04:08 -0300

    rails g specs

---
+++ spec/views/categories/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'categories/show', type: :view do
+  before(:each) do
+    @category = assign(:category, Category.create!(
+                                    name: 'Name',
+                                    icon: 'Icon',
+                                    image: 'Image',
+                                    user: nil
+                                  ))
+  end
+
+  it 'renders attributes in <p>' do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Icon/)
+    expect(rendered).to match(/Image/)
+    expect(rendered).to match(//)
+  end
+end
