accf26ea83ff51ba32a04fe42e7e625d3658b55e
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-06-29 14:02:46 -0300

    controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
 class HomeController < ApplicationController
-  def splash; end
+  protect_from_forgery with: :null_session
+  skip_before_action :authenticate_user!
+  def splash
+    redirect_to categories_path if user_signed_in?
+  end
 end
