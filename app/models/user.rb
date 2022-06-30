483ab6a63d60a4a42039e7377aacd6063c927b1b
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-06-30 14:06:03 -0300

    add select category

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  has_many :categories
+  has_many :entities, foreign_key: :user_id
 
   validates :name, presence: { message: "Name can't be blank" }
 end
